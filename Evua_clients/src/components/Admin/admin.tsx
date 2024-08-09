05f22a6ed11f145d846fb81081c0a72b1ccb32c3
Funince <neill-detan@hotmail.com>
2024-08-09 07:32:34 -0500

    elsqlmodificado

--- Evua_clients/src/components/Admin/admin.tsx
+++ Evua_clients/src/components/Admin/admin.tsx
@@ -5,37 +5,31 @@ import { RegistrosMeseros } from "./registrosMeseros";
 function AdminApp() {
   return (
     <>
-        
-          <nav className="bg-slate-400">
-            <ul>
-              <li>
-                <NavLink
-                  to="Registro"
-                  className={({ isActive }) =>
-                    isActive ? " bg-background" : ""
-                  }
-                >
-                  Registro de Meseros
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="Estadisticas"
-                  className={({ isActive }) =>
-                    isActive ? "bg-background" : ""
-                  }
-                >
-                  Estadisticas
-                </NavLink>
-              </li>
-            </ul>
-            <ModeToggle />
-          </nav>
-          <Routes>
-            <Route path="Estadisticas" element={<Estadistica />} />
-            <Route path="Registro" element={<RegistrosMeseros />} />
-          </Routes>
-       
+      <nav className="bg-slate-400">
+        <ul>
+          <li>
+            <NavLink
+              to="Registro"
+              className={({ isActive }) => (isActive ? " bg-background" : "")}
+            >
+              Registro de Meseros
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="Estadisticas"
+              className={({ isActive }) => (isActive ? "bg-background" : "")}
+            >
+              Estadisticas
+            </NavLink>
+          </li>
+        </ul>
+        <ModeToggle />
+      </nav>
+      <Routes>
+        <Route path="Estadisticas" element={<Estadistica />} />
+        <Route path="Registro" element={<RegistrosMeseros />} />
+      </Routes>
     </>
   );
 }
